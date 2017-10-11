b4a7258ecdb154443fde02ff19fba55c09b8f5a5
Ubuntu <ubuntu@alpha13.turing.ieduc.org.br>
2017-10-10 21:37:12 -0300

    primeiras classes

---
+++ src/Fornecedor.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+	String nome, idade, endereco = "R: Java ,501";
+
+	public void ImprimeNome() {
+		System.out.println("o nome é:");
+		System.out.println("Endereco: " + endereco);
+
+	}
+
+}
